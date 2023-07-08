b61c4a8a7e3bebf6703fd7e4a0ddda4b17c34788
abonic92 <120069084+abonic92@users.noreply.github.com>
2023-07-07 21:07:18 -0300

    Inicio de rama

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+// import Login from './components/login/Login';
+// import Register from './components/login/Register';
+// import Categories from './components/categories/Categories';
+// import Products from './components/products/Products';
+// import ProductDetail from './components/products/ProductDetail';
+// import CreateProduct from './components/products/CreateProduct';
+// import EditProduct from './components/products/EditProduct';
+// import CartDetail from './components/cart/CartDetail';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={Login} />
+          <Route path="/register" element={Register} />
+          <Route path="/categories" element={Categories} />
+          <Route path="/products" element={Products} />
+          <Route path="/products/:id" element={ProductDetail} />
+          <Route path="/products/create" element={CreateProduct} />
+          <Route path="/products/edit/:id" element={EditProduct} />
+          <Route path="/cart-detail" element={CartDetail} /> */}
+        </Routes>
+
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ ecommerce/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css"
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My E-commerce Store!</h1>
+      <p>Explore our wide range of products and categories.</p>
+    </div>
+  );
+}
+
+export default Home;
